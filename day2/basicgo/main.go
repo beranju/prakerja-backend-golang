3a0f9cc07608a3ec8383d3bccb0ae8c1d31eecf3
beranju <beranjusihombing1@gmail.com>
2023-10-19 20:17:25 +0700

    add day 2 folder

---
+++ day2/basicgo/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// gunakan ini jika value belum diketahui
+	var ageHuman uint = 10
+	fmt.Println(ageHuman)
+	var name = "beranju"
+	fmt.Println(name)
+
+	// gunakan ini  ketika valuenya sudah di ketahui
+	phi := 3.14
+	fmt.Println(phi)
+}
